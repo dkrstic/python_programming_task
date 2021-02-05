f833b34e5c5f852b4aa9668aa04cfc52adaca173
Dejan Krstić <dejan.krstic@netcast.rs>
2021-02-05 16:23:46 +0100

    change the response type

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ import aiohttp
 import time
 import jwt
 import uvicorn
-from fastapi import FastAPI, Request
 from datetime import date
 from urllib.parse import urljoin
+from fastapi import FastAPI, Request
+from fastapi.responses import ORJSONResponse
 
 
 TARGET_ENDPOINT = 'http://127.0.0.1:5000'
-SECRET_KEY = 'a9ddbcaba8c0ac1a0a812dc0c2f08514b23f2db0a68343cb8199ebb38a6d91e4ebfb378e22ad39c2d01 d0b4ec9c34aa91056862ddace3fbbd6852ee60c36acbf'
-USERPARAM = 'user'
+SECRET_KEY = 'a9ddbcaba8c0ac1a0a812dc0c2f08514b23f2db0a68343cb8199ebb38a6d91e4ebfb378e22ad39c2d01d0b4ec9c34aa91056862ddace3fbbd6852ee60c36acbf '
+USER_PARAM = 'user'
 USERNAME_PARAM = 'username'
 JWT_HEADER = 'x-my-jwt'
 CONTENT_TYPE_PARAM = 'CONTENT-TYPE'
@@ -33,14 +34,16 @@ async def proxy_request(request: Request):
     headers = {JWT_HEADER: jwt_token, CONTENT_TYPE_PARAM: DEFAULT_CONTENT_TYPE}
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.post(target_url, json=post_data) as res:
-            res_status = await res.status
-    return res_status
+            res_body = await res.json()
+    res_status = res.status
+    response = {"status": res_status, "message": res_body}
+    return response
 
 
 def create_jwt_token(username):
     today = date.today()
     today_date = today.strftime("%Y-%m-%d")
-    payload = {IAT_PARAM: int(time.time()), USERPARAM: username, DATE_PARAM: today_date}
+    payload = {IAT_PARAM: int(time.time()), USER_PARAM: username, DATE_PARAM: today_date}
     return jwt.encode(payload, SECRET_KEY, ALGORITHM)
 
 
