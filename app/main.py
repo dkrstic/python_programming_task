7bcb7fc0406b5f8394338327b7d468d73ef1e909
Dejan Krstić <dejan.krstic@netcast.rs>
2021-02-05 15:25:20 +0100

    define constants

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 import aiohttp
-import starlette
 import time
 import jwt
 import uvicorn
 from fastapi import FastAPI, Request
 from datetime import date
 from urllib.parse import urljoin
-from httpx import Response
 
 
 TARGET_ENDPOINT = 'http://127.0.0.1:5000'
 SECRET_KEY = 'a9ddbcaba8c0ac1a0a812dc0c2f08514b23f2db0a68343cb8199ebb38a6d91e4ebfb378e22ad39c2d01 d0b4ec9c34aa91056862ddace3fbbd6852ee60c36acbf'
+USERPARAM = 'user'
+USERNAME_PARAM = 'username'
+JWT_HEADER = 'x-my-jwt'
+CONTENT_TYPE_PARAM = 'CONTENT-TYPE'
+DEFAULT_CONTENT_TYPE = 'application/json'
+IAT_PARAM = 'iat'
+DATE_PARAM = 'date'
+ALGORITHM = "HS512"
 
 HOST = '0.0.0.0'
 PORT = 8000
@@ -22,11 +28,9 @@ app = FastAPI()
 async def proxy_request(request: Request):
     target_url = urljoin(TARGET_ENDPOINT, request.url.path)
     post_data = request.json()
-    username = post_data["username"]
-    print(request.url.path)
-    print(target_url)
+    username = post_data[USERNAME_PARAM]
     jwt_token = create_jwt_token(username)
-    headers = {"x-my-jwt": jwt_token, 'CONTENT-TYPE': 'application/json'}
+    headers = {JWT_HEADER: jwt_token, CONTENT_TYPE_PARAM: DEFAULT_CONTENT_TYPE}
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.post(target_url, json=post_data) as res:
             res_status = await res.status
@@ -36,8 +40,8 @@ async def proxy_request(request: Request):
 def create_jwt_token(username):
     today = date.today()
     today_date = today.strftime("%Y-%m-%d")
-    payload = {"iat": int(time.time()), "user": username, "date": today_date}
-    return jwt.encode(payload, SECRET_KEY, "HS512")
+    payload = {IAT_PARAM: int(time.time()), USERPARAM: username, DATE_PARAM: today_date}
+    return jwt.encode(payload, SECRET_KEY, ALGORITHM)
 
 
 if __name__ == "__main__":
